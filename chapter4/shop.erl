ab881052047e6dcb9c44d1335754a0e54c1f3413
Rodrigue A <rodrigue@lingokids.com>
2020-09-26 20:32:09 +0200

    Compute total using sum and map

--- chapter4/shop.erl
+++ chapter4/shop.erl
@@ -1,5 +1,6 @@
 -module(shop).
--export([cost/1, total/1]).
+-export([cost/1, total/1, total_fun/1]).
+-import(lists, [map/2, sum/1]).
 
 cost(oranges) -> 5;
 cost(newspaper) -> 8;
@@ -9,3 +10,6 @@ cost(milk) -> 7.
 
 total([{What, N}|T]) -> cost(What) * N + total(T);
 total([]) -> 0.
+
+total_fun(L) ->
+    lists:sum(lists:map(fun({What, N}) -> cost(What) * N end, L)).
